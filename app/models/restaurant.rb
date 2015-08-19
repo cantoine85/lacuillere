03d79ed371fcd5b33e467a945cf2631e4207869b
Claire Antoine <cantoine85@gmail.com>
2015-08-19 14:12:02 +0200

    adding restaurant validation and review validation

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,7 @@
 class Restaurant < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :address, presence: true
+  CATEGORIES = %w(chinese italian japanese french belgian)
+  validates :category, inclusion: { in: CATEGORIES }
 end
