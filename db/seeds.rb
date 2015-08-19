b5be29a8f1e62a63e0e8812159b3c14829c37bfb
Claire Antoine <cantoine85@gmail.com>
2015-08-19 15:14:49 +0200

    adding seed and restaurants and reviews controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,49 @@
+restaurants = [
+  {
+    name: "L'Avenue",
+    address: "15 rue Sidoine Apollinaire",
+    phone_number: "0658796123",
+    category: "french"
+  },
+  {
+    name: "Au Soleil Levant",
+    address: "23 rue Bonaparte",
+    phone_number: "0660334691",
+    category: "chinese"
+  },
+
+  {
+    name: "Le Maki Doré",
+    address: "90 avenue du colonel Fabien",
+    phone_number: "0678909890",
+    category: "japanese"
+
+    },
+
+  {
+    name: "Le Moule Frites",
+    address: "15 avenue de Bruxelles",
+    phone_number: "0687498789",
+    category: "belgian"
+
+    },
+  {
+    name: "Il Visconti",
+    address: "33 rue de Ballainvillier",
+    phone_number: "0658796123",
+    category: "italian"
+
+    }
+
+]
+
+Restaurant.destroy_all
+
+restaurants.each do |restaurant|
+  Restaurant.create(restaurant)
+end
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
