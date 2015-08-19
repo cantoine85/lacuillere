c73594373a0cba52bd830d0ea442890dcf57e3ba
Claire Antoine <cantoine85@gmail.com>
2015-08-19 15:43:14 +0200

    adding routes + creating index for restaurants controller and view

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,16 @@
 class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:index, :new, :create]
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
